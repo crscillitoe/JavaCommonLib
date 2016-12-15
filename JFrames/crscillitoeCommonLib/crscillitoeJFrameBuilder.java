581e219e29a7c495511c3ec2c87e9a0a1eae1b89
crscillitoe <crscillitoe@wpi.edu>
2016-12-15 16:42:15 -0500

    added JFrameBuilder

---
+++ JFrames/crscillitoeCommonLib/crscillitoeJFrameBuilder.java
@@ -0,0 +1,23 @@
+package crscillitoeCommonLib;
+
+import javax.swing.JFrame;
+
+public class crscillitoeJFrameBuilder {
+
+	/**
+	 * Returns a generic JFrame with some normal defaulted options.
+	 * @param name Name of the window
+	 * @param width Width of the window
+	 * @param height Height of the window
+	 * @return a JFrame object that is not visible, that has its defaultCloseOperation set properly.
+	 */
+	public static JFrame buildFrame(String name , int width , int height) {
+		JFrame f = new JFrame(name);
+		f.setSize(width, height);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLayout(null);
+		return f;
+	}
+	
+}
