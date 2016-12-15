3421e304eabba238863d19e0ae76cdef7a1b781f
crscillitoe <crscillitoe@wpi.edu>
2016-12-15 16:29:41 -0500

    added random() methods

--- Math/crscillitoeCommonLib/crscillitoeMath.java
+++ Math/crscillitoeCommonLib/crscillitoeMath.java
@@ -9,7 +9,25 @@ public class crscillitoeMath {
 	 * @return
 	 */
 	public static int randInt(int floor, int ceiling) {
-		return (int)(Math.random() * (ceiling - floor)) + floor;
+		return (int)(Math.random() * ((ceiling + 1) - floor)) + floor;
 	}
 	
+	/**
+	 * Returns a random double between floor and ceiling, inclusive
+	 * @param floor
+	 * @param ceiling
+	 * @return
+	 */
+	public static double random(int floor, int ceiling) {
+		return (Math.random() * ((ceiling) - floor)) + floor;
+	}
+	
+	/**
+	 * Returns a random double between 0 and ceiling, inclusive
+	 * @param ceiling
+	 * @return
+	 */
+	public static double random(int ceiling) {
+		return random(0 , ceiling);
+	}
 }
