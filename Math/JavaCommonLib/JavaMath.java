c90436339e4f64772e439e2972b4e5b553638f16
anthonydandrea <anthonydandrea2@gmail.com>
2016-12-15 23:32:00 -0500

    Adding functions
    Added hypotenus, real polysmlt, imaginary polysmlt

--- Math/JavaCommonLib/JavaMath.java
+++ Math/JavaCommonLib/JavaMath.java
@@ -4,16 +4,18 @@ public class JavaMath {
 
 	/**
 	 * Returns a random integer between floor and ceiling, inclusive
+	 * 
 	 * @param floor
 	 * @param ceiling
 	 * @return
 	 */
 	public static int randInt(int floor, int ceiling) {
-		return (int)(Math.random() * ((ceiling + 1) - floor)) + floor;
+		return (int) (Math.random() * ((ceiling + 1) - floor)) + floor;
 	}
-	
+
 	/**
 	 * Returns a random double between floor and ceiling, inclusive
+	 * 
 	 * @param floor
 	 * @param ceiling
 	 * @return
@@ -21,18 +23,21 @@ public class JavaMath {
 	public static double random(int floor, int ceiling) {
 		return (Math.random() * ((ceiling) - floor)) + floor;
 	}
-	
+
 	/**
 	 * Returns a random double between 0 and ceiling, inclusive
+	 * 
 	 * @param ceiling
 	 * @return
 	 */
 	public static double random(int ceiling) {
-		return random(0 , ceiling);
+		return random(0, ceiling);
 	}
-	
+
 	/**
-	 * Scales val from valFloor to valCeil into a new val from arbFloor to arbCeil
+	 * Scales val from valFloor to valCeil into a new val from arbFloor to
+	 * arbCeil
+	 * 
 	 * @param val
 	 * @param valFloor
 	 * @param valCeil
@@ -40,11 +45,58 @@ public class JavaMath {
 	 * @param arbCeil
 	 * @return
 	 */
-	public static float map(float val , float valFloor , float valCeil , float arbFloor , float arbCeil) {
-		
+	public static float map(float val, float valFloor, float valCeil,
+			float arbFloor, float arbCeil) {
+
 		float valDif = valCeil - valFloor;
 		float arbDif = arbCeil - arbFloor;
-		
-		return arbDif * (val/valDif);
+
+		return arbDif * (val / valDif);
 	}
+
+	public static double hypotenuse(double a, double b) {
+		return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+	}
+
+	public static double[] polysmltReal(double a, double b, double c)  {
+		if (a == 0)
+		{
+			try {
+				throw new Exception("Dividing by zero");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else if(Math.pow(b, 2) - 4 * a * c < 0)
+		{
+			try {
+				throw new Exception("Imaginary solutions");
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		double[] solutions = {
+				(-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a),
+				(-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a) };
+		return solutions;
+	}
+
+	public static String[] polysmltImaginary(double a, double b, double c) {
+
+		if (a == 0) {
+			String[] sols = { "Divide by Zero", "Divide by Zero" };
+			return sols;
+		} else {
+			String sol1 = "" + (-b / (2 * a)) + "+i*"
+					+ Math.sqrt(Math.abs(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+			String sol2 = "" + (-b / (2 * a)) + "-i*"
+					+ Math.sqrt(Math.abs(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+			String[] sols = { sol1, sol2 };
+			return sols;
+		}
+
+	}
+
 }
+
